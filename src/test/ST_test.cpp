a805b85d8bc68078c9305c0fd0621d1acec15b4c
Alessandro Polidori <polidori.1906738@studenti.uniroma1.it>
2025-02-11 18:58:07 +0100

    update

--- src/test/ST_test.cpp
+++ src/test/ST_test.cpp
@@ -17,13 +17,13 @@ using namespace std;
 
 int main() {
     // Carica le immagini
-    Image a = load_image("pano/cse/1.jpg");
-    Image b = load_image("pano/cse/2.jpg");
+    Image a = load_image("pano/rainier/1.jpg");
+    Image b = load_image("pano/rainier/2.jpg");
 
     float sigma = 1.5;
-    float thresh = 0.2;
-    int window = 10;
-    int nms = 15;  
+    float thresh = 0.3;
+    int window = 11;
+    int nms = 7;  
     
     float inlier_thresh = 5;
     int iters = 10000;
